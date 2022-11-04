b044647fe3dcd7f40455abc92e237decc6db70d0
Pinkydvs <116494570+Pinkydvs@users.noreply.github.com>
2022-11-04 17:19:17 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,7 @@
+let favoriteBooks = {title: ["A Game of Thrones", "The Hunger Games"], author: ["George R.R. Martin", "Suzzane Collins"], 
+                     year: [1996, 2008], isNewerThan2000: ["False", "True"], age: [26, 14], 
+                     characters:["Eddard Stark", "Arya Stark", "Jon Snow", "Catelyn Tully", "Katniss Everdeen",
+                     "Primrose Everdeen", "Peeta Mellark", "Haymitch Abernathy"]};
+for (let item of favoriteBooks){
+  console.log(item);
+}
