b46bf9b0a186b52013f7ed9c9177b79acfbac03c
Pinkydvs <116494570+Pinkydvs@users.noreply.github.com>
2022-11-04 17:38:25 +0200

    Update data.js

--- data.js
+++ data.js
@@ -2,6 +2,14 @@ let favoriteBooks = {title: ["A Game of Thrones", "The Hunger Games"], author: [
                      year: [1996, 2008], isNewerThan2000: ["False", "True"], age: [26, 14], 
                      characters:["Eddard Stark", "Arya Stark", "Jon Snow", "Catelyn Tully", "Katniss Everdeen",
                      "Primrose Everdeen", "Peeta Mellark", "Haymitch Abernathy"]};
+//Both book titles appear in the terminal by using a for...of loop and one console.log() inside of it.
 for (let title of favoriteBooks){
-  console.log(title);
+ console.log(title);
+}
+
+//console.log(favoriteBooks.title)
+
+//Both books' authors appear in the terminal by using a for loop and one console.log() inside of it.
+for (let i=0; i<(favoriteBooks.author).length; i++)
+{console.log(favoriteBooks.author[i]);
 }
