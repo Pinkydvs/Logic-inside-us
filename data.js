f2d1767e63212ae5d328a73653f9f55af6bb53e6
Pinkydvs <116494570+Pinkydvs@users.noreply.github.com>
2022-11-05 06:55:53 +0200

    Update data.js

--- data.js
+++ data.js
@@ -27,6 +27,6 @@ for (let j=0; j<(favoriteBooks.isNewerThan2000).length; j++)
     {if (favoriteBooks.isNewerTham2000[j]==="True")
           {console.log("This book is newer than 2000"+favoriteBooks.title[j]);
      else  
-          {console.log("This book is older than 2000"+favortieBooks.title[j]);
+          {console.log("This book is older than 2000"+favoriteBooks.title[j]);
           }
      }     
